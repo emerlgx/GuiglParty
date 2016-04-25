cc5d4a7d122abad1f05d50a3da48e070813cdb85
emerlgx <chouinard.joseph@gmail.com>
2016-04-24 20:27:05 -0400

    Corrected some syntax
    guigl is no longer spelled with an e.
    vector2 is not an array

--- Assets/Constants.cs
+++ Assets/Constants.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class Constants {
 	private static int spriteWidthPixels = 112; //ish
-	public static int guigleWidth = 1;
-	public static Vector2 gameSize = {1000, 1000}; //unity units	
+	public static int guiglWidth = 1;
+	public static Vector2 gameSize = new Vector2(1000, 1000); //unity units	
 }
 
 enum ControlCommand { Start, Stop, /*GoFast, GoSlow*/ };
