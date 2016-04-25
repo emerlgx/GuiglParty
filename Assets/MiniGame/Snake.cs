462fe57aafe965c9baae9849080a26db0191de63
emerlgx <chouinard.joseph@gmail.com>
2016-04-24 21:55:51 -0400

    Started Snake Minigame
    it has beets

---
+++ Assets/MiniGame/Snake.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Snake : MiniGame {
+	InputSet inputSet;
+
+	public GameObject SnakeHead;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+	
+	}
+
+	void tick (InputSet input) {
+		inputSet = input;
+	}
+}
