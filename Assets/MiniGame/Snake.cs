3f92d9edcd4d7ecab80efa384c6d4e9315070676
emerlgx <chouinard.joseph@gmail.com>
2016-04-25 14:57:35 -0400

    change some snake stuff
    plz don’t break

--- Assets/MiniGame/Snake.cs
+++ Assets/MiniGame/Snake.cs
@@ -5,13 +5,36 @@ public class Snake : MiniGame {
 	InputSet inputSet;
 
 	public Rigidbody2D SnakeHead;
+	public float maxSpeed = 20;		// we will not add force above this speed
+	public float rotateSpeed = 30;	// how many degrees a partyer will rotate in one second
 
 	void Start () {
-		SnakeHead.AddForce (new Vector2 (10, 10));
+		inputSet = new InputSet (true, false, false);
+
 	}
-	
-	void Update () {
-	
+
+	void FixedUpdate () {
+
+		// check the controls
+		if (inputSet.left) {
+			SnakeHead.transform.Rotate(-1.0f * Vector3.forward * rotateSpeed * Time.deltaTime);
+			Vector2 velocity = SnakeHead.velocity;
+			SnakeHead.velocity = SnakeHead.transform.InverseTransformVector(Vector2.up/8) * velocity.magnitude;
+			Debug.Log ("turned left!");
+		}
+
+		if (inputSet.right) {
+			SnakeHead.transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+			Vector2 velocity = SnakeHead.velocity;
+			SnakeHead.velocity = SnakeHead.transform.InverseTransformVector(Vector2.up/8) * velocity.magnitude;
+			Debug.Log ("Turned right!");
+		}
+
+		// move the partyer forward
+		if(SnakeHead.velocity.magnitude <= maxSpeed){
+			Vector2 forceVec = SnakeHead.transform.InverseTransformVector(Vector2.up/2);
+			SnakeHead.AddForce(forceVec);
+		}
 	}
 
 	public override void tick (InputSet input) {
@@ -19,5 +42,6 @@ public class Snake : MiniGame {
 	}
 
 	public override void control (ControlCommand command) {
+
 	}
 }
---
+++ Assets/MiniGame/Snake/SnakeFlip.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeFlip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		// reflect the snake head's orientation depending on the type of wall
+		if (coll.gameObject.tag == "horizontal") {
+			//flip
+		}
+
+		if (coll.gameObject.tag == "vertical") {
+			//flop
+		}
+
+	}
+}
